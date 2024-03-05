54cb15fcfa832b8e26c4d0c3cc88b00cb4aa25fe
PSVLOPES <psvlopes@gmail.com>
2024-03-05 10:38:34 -0300

    Envio inicio

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# automatiza_catapi
-Desafio de automação da API Votes da TheCat API
+# Projeto de automação de testes da API Votes do The CatApi
+&nbsp;
+>Este projeto de automação em RestAssured tem como objetivo realizar cenários de testes nos três verbos HTTP (GET, POST, DELETE) para a API disponibilizada no endpoint: [https://api.thecatapi.com/v1/votes](https://api.thecatapi.com/v1/votes).
+
+## Estrutura do Projeto
+
+>- **com.catapi.votes:** Contém a classe `VotesTest` que possui os cenários de testes automatizados.
+
+## ⚒️ Recursos utilizados
+
+>Certifique-se de ter as seguintes ferramentas instaladas em seu ambiente de desenvolvimento:
+### Tecnologias
+- [Java 11](https://www.java.com/)
+- [RestAssured](https://rest-assured.io/)
+- [JUnit](https://junit.org/)
+
+## Configuração do Ambiente
+
+1. Clone este repositório:
+
+```bash
+git clone https://github.com/rosessantos/desafios_meu_tudo_api.git
+```
+
+2. Abra o IntelliJ ou sua IDE preferida e importe o projeto.
+   
+
+3. Execute os testes:
+
+   - Navegue até `src/test/java/com/catapi/votes` e execute a classe `VotesTest` utilizando o JUnit.
+
+## Execução dos Testes
+
+Os testes contidos na classe `VotesTest` realizam as seguintes ações:
+
+1. **Verbo GET:** Realiza uma requisição GET para o endpoint e valida o status code (200) e o(s) votos.
+
+2. **Verbo POST:** Envia uma requisição POST com um corpo JSON e valida o status code (201) e o registro de um voto.
+
+3. **Verbo DELETE:** Envia uma requisição DELETE para um recurso específico e valida o status code (200) e se o registro do voto foi apagado.
+
+4. **JSON Schema e HTTP Code:** Realiza uma requisição GET para o endpoint e valida o JSON Schema utilizando um arquivo externo (`votes.json`) e o status code (200).
+
+
+## Autor
+
+- Pablo Lopes (Sensedia)
+
---
+++ src/test/java/com/catapi/votes/VotesTest.java
@@ -0,0 +1,222 @@
+package com.catapi.votes;
+
+
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+
+
+public class VotesTest {
+
+    // Variável global para armazenar o ID do voto
+    private static int idVote;
+
+
+    @BeforeClass
+    public static void setup() {
+        baseURI = "https://api.thecatapi.com";
+        basePath = "/v1";
+
+
+
+    }
+
+
+
+//////////////////////// CENÁRIOS PARA O POST ///////////////////////////
+
+    @Test
+    public void test01DadoUmUsuarioQuandoCadastraVoteEntaoObtenhoStatusCode200() {
+        String requestBody = "{\"image_id\":\"asf2\",\"sub_id\":\"sense2024.2\",\"value\":1}";
+
+        idVote = given()
+                .contentType("application/json")
+                .header("x-api-key", "DEMO-API-KEY")
+                .body(requestBody)
+        .when()
+                .post("/votes")
+        .then()
+                .log().all()
+                .header("content-type", "application/json; charset=utf-8")
+                .body("message" , equalTo("SUCCESS"))
+                .body("id" , notNullValue())
+                .body("image_id" , equalTo("asf2"))
+                .body("sub_id", equalTo("sense2024.2"))
+                .body("value" , equalTo(1))
+                .body("country_code" , equalTo("BR"))
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("post-votes.json"))
+                .statusCode(201)
+                .extract()
+                .path("id");
+
+
+    }
+
+    ///////////////////////// CENÁRIOS PARA O GET ALL ///////////////////////
+
+
+    @Test
+    public void test02DadoQueDesejoBuscarTodosVotosQuandoNaoPassoNenhumFiltroEFacoBuscaEntaoObtenhoStatusCode200ETodosVotos() {
+        given()
+                .contentType("application/json")
+                .header("x-api-key", "DEMO-API-KEY")
+        .when()
+                .get("/votes")
+        .then()
+                .header("x-dns-prefetch-control", equalTo("off"))
+                .header("x-frame-options", equalTo("SAMEORIGIN"))
+                .header("strict-transport-security", equalTo("max-age=15552000; includeSubDomains"))
+                .header("x-download-options", equalTo("noopen"))
+                .header("x-content-type-options", equalTo("nosniff"))
+                .header("x-xss-protection", equalTo("1; mode=block"))
+                .header("vary", equalTo("Origin"))
+                .header("content-type", equalTo("application/json"))
+                .header("pagination-page", equalTo("0"))
+                .header("pagination-limit", equalTo("100"))
+                .header("pagination-count", hasItems())
+                .body("id" , hasItems())
+                .body("image_id" , hasItems())
+                .body("sub_id" , hasItems())
+                .body("created_at" , hasItems())
+                .body("value",  hasItems())
+                .body("country_code" , hasItems())
+                .body("image.id" , hasItems())
+                .body("image.url" , hasItems())
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("get-votes.json"))
+                .statusCode(200);
+
+    };
+
+    @Test
+    public void test03DadoQueDesejoBuscarTodosVotosOrdemDecrescenteQuandoInformoFiltroDESCEntaoObtenhoStatusCode200ETodosVotosDecrescente() {
+        given()
+                .contentType("application/json")
+                .header("x-api-key", "DEMO-API-KEY")
+        .when()
+                .get("/votes?order=DESC")
+        .then()
+                .header("x-dns-prefetch-control", equalTo("off"))
+                .header("x-frame-options", equalTo("SAMEORIGIN"))
+                .header("strict-transport-security", equalTo("max-age=15552000; includeSubDomains"))
+                .header("x-download-options", equalTo("noopen"))
+                .header("x-content-type-options", equalTo("nosniff"))
+                .header("x-xss-protection", equalTo("1; mode=block"))
+                .header("vary", equalTo("Origin"))
+                .header("content-type", equalTo("application/json"))
+                .header("pagination-page", equalTo("0"))
+                .header("pagination-limit", equalTo("100"))
+                .header("pagination-count", hasItems())
+                .body("id" , hasItems())
+                .body("image_id" , hasItems())
+                .body("sub_id" , hasItems())
+                .body("created_at" , hasItems())
+                .body("value",  hasItems())
+                .body("country_code" , hasItems())
+                .body("image.id" , hasItems())
+                .body("image.url" , hasItems())
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("get-votes.json"))
+                .statusCode(200);
+
+    };
+
+    @Test
+    public void test04DadoQueNaoInformoKeyEntaoObtenhoStatusCode401() {
+        given()
+                .header("content-type","application/json")
+
+        .when()
+                .get("/votes")
+        .then()
+                .statusCode(401);
+
+    };
+
+    @Test
+    public void test05DadoUmUsuarioQuandoBuscaUmDeterminadoVotoPeloSubIDEntaoObtenhoStatusCode200EDadosDaqueleVoto() {
+        given()
+                .contentType("application/json")
+                .header("x-api-key", "DEMO-API-KEY")
+        .when()
+                .get("/votes?sub_id=sense2024")
+        .then()
+                .header("x-dns-prefetch-control", equalTo("off"))
+                .header("x-frame-options", equalTo("SAMEORIGIN"))
+                .header("strict-transport-security", equalTo("max-age=15552000; includeSubDomains"))
+                .header("x-download-options", equalTo("noopen"))
+                .header("x-content-type-options", equalTo("nosniff"))
+                .header("x-xss-protection", equalTo("1; mode=block"))
+                .header("vary", equalTo("Origin"))
+                .header("content-type", equalTo("application/json"))
+                .header("pagination-page", equalTo("0"))
+                .header("pagination-limit", equalTo("100"))
+                .header("pagination-count", hasItems())
+                .body("id" , hasItems())
+                .body("image_id" , hasItems())
+                .body("sub_id" , hasItems())
+                .body("created_at" , hasItems())
+                .body("value",  hasItems())
+                .body("country_code" , hasItems())
+                .body("image.id" , hasItems())
+                .body("image.url" , hasItems())
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("get-votes.json"))
+                .statusCode(200);
+
+    };
+
+
+
+
+    //////////////////////// CENÁRIOS PARA O GET BY ID ////////////////////
+
+    @Test
+    public void test06DadoUmUsuarioQuandoBuscaUmDeterminadoVotoPeloIDEntaoObtenhoStatusCode200EDadosDaqueleVoto() {
+        given()
+                .contentType("application/json")
+                .header("x-api-key", "DEMO-API-KEY")
+        .when()
+                .log().all()
+                .get("/votes/" + idVote)
+        .then()
+                .log().all()
+                .header("content-type", "application/json; charset=utf-8")
+                .body("id" , hasItems())
+                .body("image_id" , hasItems())
+                .body("sub_id" , hasItems())
+                .body("created_at" , hasItems())
+                .body("value",  hasItems())
+                .body("country_code" , hasItems())
+                .body("image.id" , hasItems())
+                .body("image.url" , hasItems())
+                .statusCode(200);
+
+    };
+
+    @Test
+    public void test07QuandoBuscOUmDeterminadoVotoPassandoUmIdNaoExistenteEntaoObtenhoStatusCode404() {
+        given()
+                .contentType("application/json")
+                .header("x-api-key", "DEMO-API-KEY")
+        .when()
+                .log().all()
+                .get("/votes/" + "11111111")
+        .then()
+                .log().all()
+                .header("content-type", "text/plain; charset=utf-8")
+                .statusCode(404);
+
+    };
+
+
+
+
+
+
+}
